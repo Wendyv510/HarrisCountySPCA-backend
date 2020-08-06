c17db24ad091148c6fe65dc25f2516e621565474
Wendy Harris <wendyv510@hotmail.com>
2020-08-06 14:20:46 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.create(name:'Fluffy', age:'Kitten', kind:'Long hair')
+Cat.create(name:'Zeus', age:'elderly', kind:'calico,short hair')
+
+Dog.create(name:'Spot', age:'2 years old', breed:'boxer')
+Dog.create(name:'Hillary', age:'7 years old', breed:'chihauha')
+
+Applicant.create(name:'George Smith', pet_name:'Zeus', email_address:'gsmith@gmail.com', phone_number:'8763459876', address:'34 Willow Dr. Bath,Ny 14810')
+Applicant.create(name:'Mary Jones', pet_name:'Fluffy', email_address:'mjones@gmail.com', phone_number:'8762348624', address:'987 Loft Lane Horseheads,NY 14879')
